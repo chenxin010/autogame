9eaa19986e8fc4c680783b39c98dbdedd0c84a60
chenxin <117575240@qq.com>
2025-05-30 02:16:39 +0800

    1.添加0点的限时礼包弹窗自动点击关闭 2.增加退出快捷键

--- GameControl.py
+++ GameControl.py
@@ -2,7 +2,9 @@ import win32helper
 import pyautogui
 import rapidocrhelper
 from loguru import logger
+from datetime import datetime
 
+pyautogui.FAILSAFE = False
 logger.add("file_{time}.log")
 
 handle=win32helper.GetHandle('向僵尸开炮')
@@ -16,7 +18,6 @@ def StartGame():
     if(x>0 and y>0): 
         pyautogui.click(winX+x, winY+y)
         return True
-
 
 # 返回
 def Reback():
@@ -37,7 +38,7 @@ def Elite():
     win32helper.ShotElite(handle)
     x,y=rapidocrhelper.GetPoint('精英掉落')#返回的是文字基于图片的相对坐标,还需要加上窗口坐标
     if(x>0 and y>0): 
-        pyautogui.click(winX+x, winY+y)
+        pyautogui.click(winX+ winWidth*0.5,winY+ winHeight*0.5) #点击中间空白关闭转盘
         return 1
     return 0
     
@@ -55,3 +56,13 @@ def SelectSkill():
             pyautogui.click(winX+x, winY+winHeight/2)
             break
     pyautogui.click(winX+winWidth/2,winY+winHeight/2)#点中间的
+
+# 处理0点的限时礼包
+def SkipGift():
+    now = datetime.now()
+    formatted_time = now.strftime("%H:%M")
+    if(formatted_time>'00:00' and formatted_time<'00:10'):
+        win32helper.ShotAll(handle)
+        list=rapidocrhelper.GetAll()
+        if(list.get('限时礼包','限时礼包')!='限时礼包'):
+            pyautogui.click(winX+winWidth*0.81, winY+winHeight*0.24)
--- start.py
+++ start.py
@@ -1,18 +1,26 @@
 import time
 import GameControl
+import keyboard
 
 # skills1=['子弹爆炸','连发+','分裂子弹四射']
 # skills2=['冰爆伤害','冰系缩减','连环冰暴','破裂小冰弹']
 # skills3=['干冰弹连发']
 # skills3=['热能爆炸']
+runFlag=True
 
+def stop_program():
+    print("暂停程序...")
+    global runFlag
+    runFlag = False
 
-while(True):
+keyboard.add_hotkey('ctrl+q', stop_program)
+
+while(runFlag):
     backFlag=False
-
+    GameControl.SkipGift()
     GameControl.StartGame()
 
-    while(backFlag==False):
+    while(backFlag==False and runFlag):
         if(GameControl.Elite()==1):
             continue
 
